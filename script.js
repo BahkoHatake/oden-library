e5a05caf66f30fed72da756d85acece00852ebe5
Branko Cvetanović <cbranko01@gmail.com>
2023-05-10 20:52:41 +0200

    add cancel button on new book form

--- script.js
+++ script.js
@@ -50,34 +50,43 @@ const buttonCard=document.createElement("div");
 const addButton=document.createElement("button");
 function addAddButton(){
     buttonCard.classList.add("button-card");
-    main.appendChild(buttonCard)
+    main.appendChild(buttonCard);
     addButton.classList.add("add");
     buttonCard.appendChild(addButton);
 }
 
 addButton.addEventListener("click", ()=>{
-    newBookForm.classList.add("toggle")
+    newBookForm.classList.add("toggle");
 })
+
+const newName=document.getElementById("title");
+const newAuthor=document.getElementById("author")
+const newPages=document.getElementById("pages")
+const newRead=document.getElementById("read")
 
 const newBookForm=document.getElementById("newBookForm")
 newBookForm.addEventListener("submit",(e)=>{
     e.preventDefault();
-    let newName=document.getElementById("title");
-    let newAuthor=document.getElementById("author")
-    let newPages=document.getElementById("pages")
-    let newRead=document.getElementById("read")
     let newBook= new Book(newName.value,newAuthor.value,newPages.value,newRead.checked)
     addBookToLibrary(newBook);
     displayBook();
     addAddButton();
 }) 
 
+const cancel=document.getElementById("cancel")
+cancel.addEventListener("click", ()=>{
+    newName.value="";
+    newAuthor.value="";
+    newPages.value="";
+    newBookForm.classList.remove("toggle")
+})
+
 
 
 let book1= new Book("The Wheel Of Time","Robert Jordan",654,true);
 let book2= new Book("The Great Hunt","Robert Jordan",597,false);
 let book3= new Book("Hobbit","J.R.R Tolkien",312,true);
-let book4= new Book("Malzan The Book Of Fallen","Steven Ericson",610,false);
+let book4= new Book("Malazan The Book Of Fallen","Steven Ericson",610,false);
 
 addBookToLibrary(book1);
 addBookToLibrary(book2);
